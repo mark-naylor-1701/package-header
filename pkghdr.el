d79bf5f583bae0849341325958b16988109dfeb3
Mark W. Naylor <mark.naylor.1701@gmail.com>
2017-12-21 12:00:56 -0500

    Set for packaging
    * Package headers
    * Create namespace

--- pkghdr.el
+++ pkghdr.el
@@ -1,150 +1,160 @@
+;;; pkghdr.el --- Automate some of the boilerplate needed to make an Emacs package.
 
-;; author: Mark W. Naylor
-;; file:  pkghdr.el
-;; date:  2017-Dec-18
+;; Copyright (©) 2017 Mark W. Naylor
 
-(require 'lisp-mnt)
-(require 'names)
-(require 's)
-(require 'package)
-(require 'dash)
+;; Author: Mark W. Naylor <mark.naylor.1701@gmail.com>
+;; Version: 0.9
+;; Package-Requires: ((emacs "25.0") (names "20171012.1214") (s "20171102.227") (dash "20171028.854"))
+;; Keywords: tools, convenience, package
+;; URL: https://github.com/mark-naylor-1701/package-header/
 
-(defvar -items
-  (list (cons "Author:" #'-author)
-        (cons "Version:" #'-version)
-        (cons "Package-Requires:" #'-requires)
-        (cons "Keywords:" #'-kewords)
-        (cons "URL:" #'-url)))
-(defvar comment-3 ";;;")
-(defvar comment-2 ";;")
 
-(defun add ()
-  "docstring"
-  (interactive)
-  (when (not (-header-p))
-    (save-excursion
-      (-package-header)
-      (-package-headers)
-      (-package-footer))))
+(define-namespace pkghdr-
 
-(defun -package-header ()
-  (goto-char 1)
-  (insert (-title))
-  (newline 2)
-  (insert (-copyright))
-  (newline 2)
-  )
+ (defvar -items
+   (list (cons "Author:" #'-author)
+         (cons "Version:" #'-version)
+         (cons "Package-Requires:" #'-requires)
+         (cons "Keywords:" #'-kewords)
+         (cons "URL:" #'-url)))
+ (defvar comment-3 ";;;")
+ (defvar comment-2 ";;")
 
-(defun -package-footer ()
-  (goto-char (point-max))
-  (newline 2)
-  (insert (-ender)))
+ (defun add ()
+   "docstring"
+   (interactive)
+   (when (not (-header-p))
+     (save-excursion
+       (-package-header)
+       (-package-headers)
+       (-package-footer))))
 
-(defun -package-headers ()
-  (mapc
-   #'insert
-   (mapcar #'(lambda (x) (concat x "\n"))
-           (mapcar #'-header-item -items))))
+ (defun -package-header ()
+   (goto-char 1)
+   (insert (-title))
+   (newline 2)
+   (insert (-copyright))
+   (newline 2)
+   )
 
-(defun -header-item (item)
-  (s-join " " (list comment-2 (car item) (funcall (cdr item)))))
+ (defun -package-footer ()
+   (goto-char (point-max))
+   (newline 2)
+   (insert (-ender)))
 
-(defun -copyright ()
-  (s-join " "
-          (list comment-2 "Copyright (©)" (format-time-string "%Y") (user-full-name))))
+ (defun -package-headers ()
+   (mapc
+    #'insert
+    (mapcar #'(lambda (x) (concat x "\n"))
+            (mapcar #'-header-item -items))))
 
-(defun -title ()
-  (s-join " " (list comment-3 (buffer-name) "--- DESCRIPTION")))
+ (defun -header-item (item)
+   (s-join " " (list comment-2 (car item) (funcall (cdr item)))))
 
-(defun -ender ()
-  (s-join " " (list comment-3 (buffer-name) "ends here")))
+ (defun -copyright ()
+   (s-join " "
+           (list comment-2 "Copyright (©)" (format-time-string "%Y") (user-full-name))))
 
-(defun -header-p ()
-  (save-excursion
-    (goto-char (point-min))
-    (move-end-of-line 1)
-    (narrow-to-region 1 (point))
-    (goto-char (point-min))
-    (let ((res (search-forward-regexp "^;;; .+\.el --- .*" nil t)))
-      (widen)
-      res)))
+ (defun -title ()
+   (s-join " " (list comment-3 (buffer-name) "--- DESCRIPTION")))
 
-(defun -author ()
-  (s-join " " (list (user-full-name) (-angle-brack user-mail-address))))
+ (defun -ender ()
+   (s-join " " (list comment-3 (buffer-name) "ends here")))
 
-(defun -angle-brack (string)
-  (s-join "" (list "<" string ">")))
+ (defun -header-p ()
+   (save-excursion
+     (goto-char (point-min))
+     (move-end-of-line 1)
+     (narrow-to-region 1 (point))
+     (goto-char (point-min))
+     (let ((res (search-forward-regexp "^;;; .+\.el --- .*" nil t)))
+       (widen)
+       res)))
 
-(defun -version ()
-  "0.9")
+ (defun -author ()
+   (s-join " " (list (user-full-name) (-angle-brack user-mail-address))))
 
-(defun -listify (s)
-  (s-join
-   ""
-   (list "(" s ")")))
+ (defun -angle-brack (string)
+   (s-join "" (list "<" string ">")))
 
-(defun -stringify (s)
-  (s-join ""
-          (list "\"" s "\"")))
+ (defun -version ()
+   "0.9")
 
-(defun -emacs-version ()
-  (cons
-   'emacs
+ (defun -listify (s)
    (s-join
-    "."
-    (mapcar
-     #'int-to-string
-     (list emacs-major-version 0)))))
+    ""
+    (list "(" s ")")))
 
-(defun -requires ()
-  (-listify (-emacs-version)))
+ (defun -stringify (s)
+   (s-join ""
+           (list "\"" s "\"")))
 
-(defun -kewords ()
-  "foo bar baz")
+ (defun -emacs-version ()
+   (cons
+    'emacs
+    (s-join
+     "."
+     (mapcar
+      #'int-to-string
+      (list emacs-major-version 0)))))
 
-(defun -url ()
-  "http://example.com/jrhacker/superfrobnicate")
+ (defun -requires ()
+   (-format-pagkages-versions))
 
-(defun -find-package-name ()
-  (when (re-search-forward "(require[ \\t]'" nil t)
-    (symbol-at-point)))
+ (defun -kewords ()
+   "foo bar baz")
+
+ (defun -url ()
+   "http://example.com/jrhacker/superfrobnicate")
+
+ (defun -find-package-name ()
+   (when (re-search-forward "(require[ \\t]'" nil t)
+     (symbol-at-point)))
 
 
-(defun -find-package-names ()
-  "Search forward from the point, accumulating a list of all
+ (defun -find-package-names ()
+   "Search forward from the point, accumulating a list of all
 symbol names for explicitly required packages."
-  (cl-labels ((inner (lst)
-                       (let ((name (-find-package-name)))
-                         (cond ((null name) lst)
-                               (t (inner (cons name lst)))))))
-    (nreverse (inner '()))))
+   (cl-labels ((inner (lst)
+                      (let ((name (-find-package-name)))
+                        (cond ((null name) lst)
+                              (t (inner (cons name lst)))))))
+     (nreverse (inner '()))))
 
-(defun -package-version (pkg)
-  (with-temp-buffer
-    (ignore-errors
-      (describe-package-1 pkg)
-      (goto-char 1)
-      (when (re-search-forward "Status.*-\\(.*\\)/" nil t)
-        (match-string-no-properties 1)))))
+ (defun -package-version (pkg)
+   (with-temp-buffer
+     (ignore-errors
+       (describe-package-1 pkg)
+       (goto-char 1)
+       (when (re-search-forward "Status.*-\\(.*\\)/" nil t)
+         (match-string-no-properties 1)))))
 
-(defun -explicit-packages-and-versions ()
-  (save-excursion
-    (goto-char 1)
-    (let ((pkgs (-find-package-names)))
-      (-filter #'cdr (-zip pkgs (mapcar #'-package-version pkgs))))))
+ (defun -explicit-packages-and-versions ()
+   (save-excursion
+     (goto-char 1)
+     (let ((pkgs (-find-package-names)))
+       (-filter #'cdr (-zip pkgs (mapcar #'-package-version pkgs))))))
 
-(defun -packages-and-versions ()
-  (cons (-emacs-version) (-explicit-packages-and-versions)))
+ (defun -packages-and-versions ()
+   (cons (-emacs-version) (-explicit-packages-and-versions)))
 
-(defun -format-package (pkg)
-  (symbol-name pkg))
+ (defun -format-package (pkg)
+   (symbol-name pkg))
 
-(defun -format-version (ver)
-  (-stringify ver))
+ (defun -format-version (ver)
+   (-stringify ver))
 
-(defun -format-package-version (pkg-ver)
-  (-listify
-   (s-join
-    " "
-    (list (-format-package (car pkg-ver))
-          (-format-version (cdr pkg-ver))))))
+ (defun -format-package-version (pkg-ver)
+   (-listify
+    (s-join
+     " "
+     (list (-format-package (car pkg-ver))
+           (-format-version (cdr pkg-ver))))))
+ (defun -format-pagkages-versions ()
+   (-listify
+    (s-join
+     " "
+     (mapcar #'-format-package-version (-packages-and-versions)))))
+
+ ) ; end of namspace definition
+;;; pkghdr.el ends here
